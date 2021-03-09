be8d96338bd57e63f97a2ea80d76f644e0f0d9de
黑咖啡 <2452052376@qq.com>
2021-03-09 21:57:24 +0800

    fix: 钉钉登录扫码

--- src/lib/remote-js/index.js
+++ src/lib/remote-js/index.js
@@ -1,19 +1,17 @@
 export default {
-  components: {
-    'remote-js': {
-      render(createElement) {
-        return createElement('script', {
-          attrs: {
-            type: 'text/javascript',
-            src: this.src
-          }
-        })
-      },
-      props: {
-        src: {
-          type: String,
-          required: true
+  'remote-js': {
+    render(createElement) {
+      return createElement('script', {
+        attrs: {
+          type: 'text/javascript',
+          src: this.src
         }
+      })
+    },
+    props: {
+      src: {
+        type: String,
+        required: true
       }
     }
   }
--- src/packages/web/dingtalk/index.vue
+++ src/packages/web/dingtalk/index.vue
@@ -1,26 +1,77 @@
 <template>
-  <div class="dingtalk">
-    <remote-js src="https://g.alicdn.com/dingding/dinglogin/0.0.5/ddLogin.js" />
+  <div v-loading="loading" class="dingtalk">
+    <div id="login-container" />
   </div>
 </template>
 <script>
-import remoteJs from '../../../lib/remote-js'
 export default {
   name: 'Dingtalk',
-  components: { remoteJs },
+  components: {},
   props: {},
   data() {
-    return {}
+    return {
+      loading: true,
+      appid: 'dingoa1qzpp6zazbfqbc8y', // 扫描授权ID
+      redirect_url: 'https://ding-doc.dingtalk.com/', // 设置重定向地址
+      config: {}
+    }
   },
   computed: {},
   watch: {},
-  mounted() {},
-  methods: {}
+  mounted() {
+    this.initSrc()
+    this.initConfig()
+  },
+  methods: {
+    initSrc() {
+      this.loading = true
+      const oScript = document.createElement('script')
+      oScript.type = 'text/javascript'
+      oScript.src = 'https://g.alicdn.com/dingding/dinglogin/0.0.5/ddLogin.js'
+      document.body.appendChild(oScript)
+      setTimeout(() => {
+        this.initConfig()
+      }, 10 * 1000)
+    },
+    initConfig() {
+      const textUrl = 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid='
+      const params = '&response_type=code&scope=snsapi_login&state=STATE&redirect_uri='
+      const url = textUrl + this.appid + params + this.redirect_url
+      if (url && encodeURIComponent(url)) {
+        DDLogin({
+          id: 'login-container',
+          goto: encodeURIComponent(url),
+          style: 'border:none;background-color:#FFFFFF;',
+          width: '365',
+          height: '400'
+        })
+        this.loading = false
+        var getCode = function(event) {
+          var origin = event.origin
+          if (origin === 'https://login.dingtalk.com') {
+            const tmpCode = event.data
+            if (tmpCode && url) {
+              window.location.href = url + `&loginTmpCode=${tmpCode}`
+            }
+          }
+        }
+        if (typeof window.addEventListener !== 'undefined') {
+          window.addEventListener('message', getCode, false)
+        } else if (typeof window.attachEvent !== 'undefined') {
+          window.attachEvent('onmessage', getCode)
+        }
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 .dingtalk {
   width: 100%;
   height: 100%;
+  #login-container {
+    display: flex;
+    justify-content: center;
+  }
 }
 </style>
